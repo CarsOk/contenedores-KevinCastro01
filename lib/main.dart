58df255ff77a16fbb8b5ef54a13e66c1c1b42298
Kevin <kevinmcf04@gmail.com>
2021-05-11 15:18:46 -0500

    Colocar iconos de las redes sociales

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Mi primer juego',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
@@ -24,28 +21,29 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Bienvenidos a mi app')),
       body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text('Creador: Kevin Castro',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30.0,
-                  color: Colors.green,
-                )),
-            Icon(
-              Icons.gamepad,
-              color: Colors.green,
-              size: 100.0,
-            ),
-            new Image.asset(
-              'assets/logo1.png',
-              width: 300.0,
-              height: 300.0,
-            )
-          ],
-        ),
-      ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset('assets/logo1.png'),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset(
+                'assets/whatsapp.png',
+                width: 40.0,
+              ),
+              Image.asset(
+                'assets/fb.png',
+                width: 30.0,
+              ),
+              Image.asset(
+                'assets/ig.png',
+                width: 30.0,
+              ),
+            ],
+          )
+        ],
+      )),
     );
   }
 }
