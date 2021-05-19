653d51a1d7de84a9d9ed3e04991a1ff6e99f1d6e
Kevin <kevinmcf04@gmail.com>
2021-05-18 21:22:15 -0500

    Consumir API

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,64 +12,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
       ),
       home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Bienvenidos a mi app')),
-      body: Container(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Image.asset('assets/logo1.png'),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Image.asset(
-                'assets/whatsapp.png',
-                width: 40.0,
-              ),
-              Image.asset(
-                'assets/fb.png',
-                width: 30.0,
-              ),
-              Image.asset(
-                'assets/ig.png',
-                width: 30.0,
-              ),
-            ],
-          )
-        ],
-      )),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text('Aqui va a ir una imagen'),
-            ),
-            ListTile(
-              title: Text('Juegos gratis'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Contactanos'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
